9c96604ccad5e11a26215a911ae130f041e545d6
Ruiqian1 <yaoruiqian@gmail.com>
2018-05-07 10:26:36 -0500

    dump data test

--- bench/model_trainer.cxx
+++ bench/model_trainer.cxx
@@ -248,7 +248,10 @@ void train_all(double time, World & dw, bool write_coeff, bool dump_data, std::s
    if(write_coeff)
       CTF_int::write_all_models(coeff_file);
    if(dump_data){
-      std::cout<<"YAYYYYYYYYYYYYYYYY!!! We are dumping the data! YAYYYYYYYYYYYYYYYY!"<<std::endl;
+      int rank, np;
+      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+      MPI_Comm_size(MPI_COMM_WORLD, &np);
+      std::cout<<rank<<"/"<<np<<" "<<"YAYYYYYYYYYYYYYYYY!!! We are dumping the data! YAYYYYYYYYYYYYYYYY!"<<std::endl;
       CTF_int::dump_all_models(data_dir);
    }
 
